ab3267af1e7e213f1b288a449f77e04967402e46
Adam El-Mobdy <adamelmobdy@gmail.com>
2023-10-22 13:27:54 -0700

    Created a way for the user to print to the screen

--- texttango/frontend/src/components/app.js
+++ texttango/frontend/src/components/app.js
@@ -1,15 +1,44 @@
-import React, {Component} from "react";
-import { render } from "react-dom";
+import ReactDom from 'react-dom';
+import React, {useState, useEffect} from 'react';
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
+function App(){
+    const[data,setData] = useState(null);
+    const[print,setPrint] = useState(false);
+
+    useEffect(() => {
+        if (print) {
+            console.log(data);
+        }
+    }, [print, data]);
+
+    function getData(val)
+    {
+        setData(val.target.value);
+        setPrint(false);
+        console.warn(val.target.value);
+    }
+    return(
+        <div className="App">
+            {
+                print?
+                <h1>{data}</h1>
+                :null
+            }
+            <h1>Get Input Box value</h1>
+            <input type="text" onChange={getData}/>
+            <button onClick={()=>setPrint(true)}>Print Value</button>
+        </div>
+    )
+
+}
+window.onload = function()
+{
+    const root = document.getElementById('root')
+    if(root){
+        ReactDom.createRoot(root).render(<App />);
+
     }
 
-    render(){
-        return (<h1>TEST</h1>);
-    }
+
 }
 
-const appDiv = document.getElementById("app");
-render(<App />,appDiv);
